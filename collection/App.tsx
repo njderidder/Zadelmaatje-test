2002693255efce23066626b1fe03763de437e6e6
njderidder <njderidder@hotmail.com>
2025-12-10 22:19:05 +0100

    Update App.tsx

--- collection/App.tsx
+++ collection/App.tsx
@@ -1,25 +1,22 @@
-// src/App.tsx of waar hij nu ook staat
-import React from 'react';
-import Navigation from './components/Navigation';
-import Hero from './components/Hero';
-import ProductScroll from './components/ProductScroll';
-import Features from './components/Features';
-import Footer from './components/Footer';
-import { EditableComponent } from '@cloudcannon/editable-regions/react';
+import React from "react";
+import Navigation from "./components/Navigation";
+import Hero from "./components/Hero";
+import ProductScroll from "./components/ProductScroll";
+import Features from "./components/Features";
+import Footer from "./components/Footer";
+import { EditableComponent } from "@cloudcannon/editable-regions/react";
 
 const App: React.FC = () => {
   return (
     <div className="bg-brand-dark min-h-screen text-white bg-noise selection:bg-brand-accent selection:text-white">
       <Navigation />
       <main>
-        {/* Component Editable Region voor de hele Hero */}
-        <EditableComponent
-          data-component="hero"
-          data-prop='@file[src/constants.ts].HERO_COPY'
-        >
+        {/* Hero als component editable region */}
+        <EditableComponent data-component="hero">
           <Hero />
         </EditableComponent>
 
+        {/* De rest kan later ook editable gemaakt worden */}
         <ProductScroll />
         <Features />
       </main>
@@ -29,4 +26,3 @@ const App: React.FC = () => {
 };
 
 export default App;
-
