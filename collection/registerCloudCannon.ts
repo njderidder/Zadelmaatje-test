7edd41385baca57a122f05129e7a573e6a92d5e3
njderidder <njderidder@hotmail.com>
2025-12-10 22:32:20 +0100

    Update registerCloudCannon.ts

--- collection/registerCloudCannon.ts
+++ collection/registerCloudCannon.ts
@@ -1,16 +1,17 @@
-// registerCloudCannon.ts
+// collection/registerCloudCannon.ts
 import { registerReactComponent } from "@cloudcannon/editable-regions/react";
 
-import Hero from "./components/Hero";
-import Features from "./components/Features";
-import Navigation from "./components/Navigation";
-import Footer from "./components/Footer";
-import ProductScroll from "./components/ProductScroll";
+import Hero from "../components/Hero";
+import Features from "../components/Features";
+import Navigation from "../components/Navigation";
+import Footer from "../components/Footer";
+import ProductScroll from "../components/ProductScroll";
 
-// De namen hieronder gebruik je straks in data-component
+// De namen hieronder gebruik je in data-component
 registerReactComponent("hero", Hero);
 registerReactComponent("features", Features);
 registerReactComponent("navigation", Navigation);
 registerReactComponent("footer", Footer);
 registerReactComponent("productScroll", ProductScroll);
+
 
