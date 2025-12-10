ac50922ec19b0f687e951cadadde4a4b2b601aa3
njderidder <njderidder@hotmail.com>
2025-12-10 21:39:16 +0100

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,16 +1,25 @@
+// src/App.tsx of waar hij nu ook staat
 import React from 'react';
 import Navigation from './components/Navigation';
 import Hero from './components/Hero';
 import ProductScroll from './components/ProductScroll';
 import Features from './components/Features';
 import Footer from './components/Footer';
+import { EditableComponent } from '@cloudcannon/editable-regions/react';
 
 const App: React.FC = () => {
   return (
     <div className="bg-brand-dark min-h-screen text-white bg-noise selection:bg-brand-accent selection:text-white">
       <Navigation />
       <main>
-        <Hero />
+        {/* Component Editable Region voor de hele Hero */}
+        <EditableComponent
+          data-component="hero"
+          data-prop='@file[src/constants.ts].HERO_COPY'
+        >
+          <Hero />
+        </EditableComponent>
+
         <ProductScroll />
         <Features />
       </main>
@@ -20,3 +29,4 @@ const App: React.FC = () => {
 };
 
 export default App;
+
