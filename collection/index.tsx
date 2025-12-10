37e43902042a96f3e33e480589d22e907edd7bd2
njderidder <njderidder@hotmail.com>
2025-12-10 23:35:21 +0100

    Update index.tsx

--- collection/index.tsx
+++ collection/index.tsx
@@ -2,16 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-// Alleen in de CloudCannon editor laadt hij de registratie
-if (typeof window !== "undefined" && (window as any).inEditorMode) {
-  import("./registerCloudCannon").catch((error) => {
-    console.warn(
-      "Failed to load CloudCannon component registration",
-      error
-    );
-  });
-}
-
 const rootElement = document.getElementById("root");
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
@@ -23,3 +13,4 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
